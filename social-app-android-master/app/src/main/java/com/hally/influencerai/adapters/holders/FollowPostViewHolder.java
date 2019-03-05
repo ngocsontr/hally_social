4703b70f19c680b99bc29bee1ea269618c76a5bb
Khang <khangtv@fabbi.com.vn>
2019-03-05 16:07:58 +0700

    init

---
+++ social-app-android-master/app/src/main/java/com/hally/influencerai/adapters/holders/FollowPostViewHolder.java
@@ -0,0 +1,39 @@
+package com.hally.influencerai.adapters.holders;
+
+import android.view.View;
+
+import com.hally.influencerai.main.base.BaseActivity;
+import com.hally.influencerai.managers.listeners.OnPostChangedListener;
+import com.hally.influencerai.model.FollowingPost;
+import com.hally.influencerai.model.Post;
+import com.hally.influencerai.utils.LogUtil;
+
+/**
+ * Created by Alexey on 22.05.18.
+ */
+public class FollowPostViewHolder extends PostViewHolder {
+
+
+    public FollowPostViewHolder(View view, OnClickListener onClickListener, BaseActivity activity) {
+        super(view, onClickListener, activity);
+    }
+
+    public FollowPostViewHolder(View view, OnClickListener onClickListener, BaseActivity activity, boolean isAuthorNeeded) {
+        super(view, onClickListener, activity, isAuthorNeeded);
+    }
+
+    public void bindData(FollowingPost followingPost) {
+        postManager.getSinglePostValue(followingPost.getPostId(), new OnPostChangedListener() {
+            @Override
+            public void onObjectChanged(Post obj) {
+                bindData(obj);
+            }
+
+            @Override
+            public void onError(String errorText) {
+                LogUtil.logError(TAG, "bindData", new RuntimeException(errorText));
+            }
+        });
+    }
+
+}
---
+++ social-app-android-master/app/src/main/java/com/hally/influencerai/main/usersList/UsersListType.java
@@ -0,0 +1,9 @@
+package com.hally.influencerai.main.usersList;
+
+/**
+ * Created by Alexey on 16.05.18.
+ */
+public interface UsersListType {
+    int FOLLOWINGS = 115;
+    int FOLLOWERS = 116;
+}
